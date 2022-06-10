2f2460ac347be4b78beb1c79c7ced7099d3e26db
Egor Demeshko <demeshko.egor@gmail.com>
2022-06-10 22:49:49 +0300

    реализация спам фильтра

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,10 @@
 function checkSpam(str) {
-  // ваш код...
+  const xBet = "1xbet";
+  const xxx = "xxx";
+
+  str = str.toLowerCase();
+
+  if ( str.includes(xBet) || str.includes(xxx) ) return true;
+
+  return false;
 }
