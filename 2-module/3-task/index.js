acd3212165f58e16b8a40c2a73998fde77668ce1
Egor Demeshko <demeshko.egor@gmail.com>
2022-06-14 21:14:56 +0300

    решение 3-ей задачи

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,35 @@
 let calculator = {
-  // ваш код
+  read(x, y){
+    if ( typeof x == 'number' && typeof y == 'number' ){
+    this.x = x;
+    this.y = y;
+    }
+    return 'аргументы - не цифровые значения';
+  },
+
+
+  sum(){
+    if ( !isEmpty(this) ){
+      return this.x + this.y;
+    }
+    return 'не заданы "x" "y", запустите метод read(x, y)'
+  },
+
+
+  mul(){
+    if ( !isEmpty(this) ){
+      return this.x * this.y;
+    }
+    return 'не заданы "x" "y", запустите метод read(x, y)'
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
+
+
+function isEmpty(obj) {
+  let objKeys = Object.keys( obj );
+
+  return ( !objKeys[0] ) ? true : false;
+}
