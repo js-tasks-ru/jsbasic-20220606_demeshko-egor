da7f8cea8e582ae1de48e05b2908510300fd1543
Egor Demeshko <demeshko.egor@gmail.com>
2022-06-17 22:55:43 +0300

    решение задачи 4

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,17 @@
 function showSalary(users, age) {
-  // ваш код...
+  let answerStr = '';
+  let usersApproved = [];
+
+  for (let user of users) {
+    if (user.age <= age) usersApproved.push(user);
+  }
+
+  for (let i = 0; i < usersApproved.length; i++){
+    let user = usersApproved[i];
+
+    answerStr += `${user.name}, ${user.balance}`;
+    answerStr += ( i == usersApproved.length - 1 ) ? '' : '\n';
+  }
+
+  return answerStr;
 }
