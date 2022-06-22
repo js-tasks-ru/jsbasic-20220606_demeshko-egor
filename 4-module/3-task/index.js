1504fa29f422aec3c51cacec0ff7d76a548e251a
Egor Demeshko <demeshko.egor@gmail.com>
2022-06-22 23:03:23 +0300

    решение 3-ьей задачи 4-го модуля

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,23 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 1; i < table.rows.length; i++){
+     let row = table.rows[i];
+
+     if (+row.cells[1].textContent < 18) {
+       row.style.textDecoration = 'line-through';
+     }
+
+     if (row.cells[2].textContent && row.cells[2].textContent == 'f') {
+       row.classList.add('female');
+     } else if (row.cells[2].textContent){
+       row.classList.add('male');
+     }
+
+     switch (row.cells[3].dataset.available){
+       case 'true' : row.classList.add('available');
+       break;
+       case 'false' : row.classList.add('unavailable');
+       break;
+       default: row.setAttribute('hidden', true);
+     }
+  }
 }
