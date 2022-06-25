9926eda9541e3df4601b9e3b962d01b72747b964
Egor Demeshko <demeshko.egor@gmail.com>
2022-06-25 18:04:47 +0300

    решение первой задачи 5-го модуля

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,10 @@
 function hideSelf() {
-  // ваш код...
+   document.documentElement.addEventListener('click', hideButton);
+}
+
+function hideButton(event){
+  const target = event.target;
+  if (target.tagName === 'BUTTON'){
+    target.hidden = true;
+  }
 }
