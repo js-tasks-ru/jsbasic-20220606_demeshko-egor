c26d94d69cffd0345881c4ed94bb758c9b2fd732
Egor Demeshko <demeshko.egor@gmail.com>
2022-07-03 19:46:24 +0300

    решение первой задачи

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,64 @@
  *
  */
 export default class UserTable {
+  #table = document.createElement('table');
+
   constructor(rows) {
+    this.rows = rows;
+    this.elem;
+    this.#createTableHead();
+    this.#fillMainTable();
+    this.#addClickListener();
   }
+
+  get elem(){
+    return this.#table;
+  }
+
+  #createTableHead(){
+    if(this.#table.childElementCount > 0) return;
+
+    const headRow = "<tr><th>Имя</th><th>Возраст</th><th>Зарплата</th><th>Город</th><th></th></tr>";
+    const tHead = document.createElement('tHead');
+
+    tHead.innerHTML = headRow;
+    this.#table.append(tHead);
+  }
+
+  #fillMainTable(){
+    const tBody = document.createElement('tBody');
+
+    if (this.rows.length == 0) return;
+
+    for (let worker of this.rows){
+      let rowWorkerData;
+
+      rowWorkerData = this.createRow(worker);
+      tBody.append(rowWorkerData);
+    }
+
+    this.#table.append(tBody);
+  }
+
+  createRow(obj){
+    if(!obj.name || !obj.age) console.log('неправильный формат данныx. требуется {name, age, salary, city}');
+
+    let {name, age, salary, city} = obj;
+    const rowElement = document.createElement('tr');
+    const rowInnerString = `<td>${name}</td><td>${age}</td><td>${salary}</td><td>${city}</td><td><button>X</button></td>`;
+
+    rowElement.innerHTML = rowInnerString;
+    return rowElement;
+  }
+
+  #addClickListener(){
+    this.#table.addEventListener('click', (event)=>{
+      let target= event.target;
+
+      if(target.tagName != 'BUTTON') return;
+
+      target.closest('tr').remove();
+    });
+  }
+
 }
