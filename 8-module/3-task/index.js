8f4cafb9c54ba5117e4c3245d27ae93617295a58
Egor Demeshko <demeshko.egor@gmail.com>
2022-07-28 21:57:26 +0300

    решение третьей задачи 8-го модуля

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -5,30 +5,95 @@ export default class Cart {
     this.cartIcon = cartIcon;
   }
 
+
   addProduct(product) {
-    // ваш код
+    if(!product) return;
+    let productToAdd = null;
+
+    if (this.cartItems.length != 0){
+      for (let object of this.cartItems){
+        if(object.product.id == product.id){
+          object.count += 1;
+        } else {
+          productToAdd = product;
+        }
+      }
+    } else {
+      this.addNewProduct(product);
+    }
+
+    if (productToAdd){
+      this.addNewProduct(productToAdd);
+    }
   }
+
+
+  addNewProduct(product){
+    const cartItemsKeys = ['product', 'count'];
+
+    let cartItem = {};
+    cartItem[cartItemsKeys[0]] = product;
+    cartItem[cartItemsKeys[1]] = 1;
+    this.cartItems.push(cartItem);
+    this.onProductUpdate(cartItem);
+  }
+
 
   updateProductCount(productId, amount) {
-    // ваш код
+    debugger;
+    if(productId === null) return;
+    this.cartItems.find( (cartItem, index, arr) => {
+      if(!cartItem) return;
+      
+      if(cartItem.product.id === productId) {
+        cartItem.count += amount;
+
+        if (cartItem.count == 0){
+          for(let i = index; i < arr.length-1; i++){
+            arr[i] = arr[i+1];
+          }
+
+        arr.pop();
+        }
+
+        this.onProductUpdate(cartItem);
+      };
+    });
   }
+
 
   isEmpty() {
-    // ваш код
+    if(this.cartItems.length == 0) return true;
+    return false;
   }
+
 
   getTotalCount() {
-    // ваш код
+    let count = 0;
+
+    for(let cartItem of this.cartItems){
+      if (!cartItem) return 0;
+      count += cartItem.count;
+    }
+
+    return count;
   }
 
+
   getTotalPrice() {
-    // ваш код
+    let totalPrice = 0;
+
+    for(let cartItem of this.cartItems){
+      if(!cartItem) return 0;
+      totalPrice += cartItem.count * cartItem.product.price;
+    }
+
+    return totalPrice;
   }
 
   onProductUpdate(cartItem) {
     // реализуем в следующей задаче
-
+    console.log(`onProductUpdate(${cartItem})`);
     this.cartIcon.update(this);
   }
 }
-
